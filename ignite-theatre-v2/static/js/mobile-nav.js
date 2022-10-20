6045279bab74bf99205e85e60ef4ba9bd773dae3
Bealby <mgbealby@gmail.com>
2022-10-20 10:51:16 +0200

    Nav functionality. Set-up URL/viiews

---
+++ ignite-theatre-v2/static/js/mobile-nav.js
@@ -0,0 +1,8 @@
+// Full Screen Overlay Responsive Nav Bar Mobile
+
+function closeMenu(){
+  document.getElementById("navbar").style.height = "0%";
+}
+function openMenu(){
+  document.getElementById("navbar").style.height = "100%";
+}
