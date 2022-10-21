70744d1719830a0967ba2467be205481eb28bdaf
Bealby <mgbealby@gmail.com>
2022-10-21 09:47:03 +0200

    Add comments to jas file

--- ignite-theatre-v2/static/js/mobile-nav.js
+++ ignite-theatre-v2/static/js/mobile-nav.js
@@ -1,29 +1,36 @@
-// Full Screen Overlay Responsive Nav Bar Mobile
+// FULL SCREEN OVERLAY FOR MOBILE
 
+// Open navbar when toggle clicked on mobile
 function openMenu(){
   document.getElementById("navbar").style.height = "100%";
 }
 
+// Open archive menu when clicked
 function openMenuArchive(){
   document.getElementById("navbar-archive").style.height = "100%";
 }
 
+// Open gallery menu when clicked
 function openMenuGallery(){
   document.getElementById("navbar-gallery").style.height = "100%";
 }
 
+// Close menu for mobile
 function closeMenu(){
   document.getElementById("navbar").style.height = "0%";
 }
 
+// Close gallery menu when "back" button clicked
 function closeMenuGallery (){
   document.getElementById("navbar-gallery").style.height = "0%";
 }
 
+// Close archive menu when "back" button clicked
 function closeMenuArchive (){
   document.getElementById("navbar-archive").style.height = "0%";
 }
 
+// Close all menus when cross clicked
 function closeAllMenu (){
   document.getElementById("navbar-gallery").style.height = "0%";
   document.getElementById("navbar").style.height = "0%";
