9c09d699ffea0a230dcfbffe709e46f36bb46884
Bealby <mgbealby@gmail.com>
2022-10-21 09:36:10 +0200

    Mob nav functionality. JS and HTML

--- ignite-theatre-v2/static/js/mobile-nav.js
+++ ignite-theatre-v2/static/js/mobile-nav.js
@@ -1,16 +1,31 @@
 // Full Screen Overlay Responsive Nav Bar Mobile
 
-function closeMenu(){
-  document.getElementById("navbar").style.height = "0%";
-}
 function openMenu(){
   document.getElementById("navbar").style.height = "100%";
 }
 
-function closeMenuGallery (){
-  document.getElementById("navbar-gallery").style.height = "0%";
-  document.getElementById("navbar").style.height = "0%";
+function openMenuArchive(){
+  document.getElementById("navbar-archive").style.height = "100%";
 }
+
 function openMenuGallery(){
   document.getElementById("navbar-gallery").style.height = "100%";
+}
+
+function closeMenu(){
+  document.getElementById("navbar").style.height = "0%";
+}
+
+function closeMenuGallery (){
+  document.getElementById("navbar-gallery").style.height = "0%";
+}
+
+function closeMenuArchive (){
+  document.getElementById("navbar-archive").style.height = "0%";
+}
+
+function closeAllMenu (){
+  document.getElementById("navbar-gallery").style.height = "0%";
+  document.getElementById("navbar").style.height = "0%";
+  document.getElementById("navbar-archive").style.height = "0%";
 }
