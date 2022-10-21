778b42b21c54bf88bb306bedea8d58752b9c04ae
Bealby <mgbealby@gmail.com>
2022-10-21 11:15:41 +0200

    Change name of js fucntions. Change text fo navbar

--- ignite-theatre-v2/static/js/mobile-nav.js
+++ ignite-theatre-v2/static/js/mobile-nav.js
@@ -5,9 +5,9 @@ function openMenu(){
   document.getElementById("navbar").style.height = "100%";
 }
 
-// Open archive menu when clicked
-function openMenuArchive(){
-  document.getElementById("navbar-archive").style.height = "100%";
+// Open cast menu when clicked
+function openMenuCast(){
+  document.getElementById("navbar-cast").style.height = "100%";
 }
 
 // Open gallery menu when clicked
@@ -25,14 +25,14 @@ function closeMenuGallery (){
   document.getElementById("navbar-gallery").style.height = "0%";
 }
 
-// Close archive menu when "back" button clicked
-function closeMenuArchive (){
-  document.getElementById("navbar-archive").style.height = "0%";
+// Close cast menu when "back" button clicked
+function closeMenuCast (){
+  document.getElementById("navbar-cast").style.height = "0%";
 }
 
 // Close all menus when cross clicked
 function closeAllMenu (){
   document.getElementById("navbar-gallery").style.height = "0%";
   document.getElementById("navbar").style.height = "0%";
-  document.getElementById("navbar-archive").style.height = "0%";
+  document.getElementById("navbar-cast").style.height = "0%";
 }
