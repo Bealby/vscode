6394a5b3beba598cff9e1db2ef332b2dc27acbb4
Bealby <mgbealby@gmail.com>
2022-10-22 09:01:03 +0200

    Set-up views and image template

--- ignite-theatre-v2/gallery/views.py
+++ ignite-theatre-v2/gallery/views.py
@@ -6,10 +6,10 @@ def all_gallery(request):
     '''A view to show all images'''
     '''context = to allow things to be sent to template'''
     
-    gallery = Image.object.all()
+    images = Image.objects.all()
 
     context = {
-        'gallery': gallery,
+        'images': images
     }
 
     return render(request, 'gallery/gallery.html', context)
