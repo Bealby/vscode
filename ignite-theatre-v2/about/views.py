367faaf749ff56035fd0d0eb39a7bd3ab24b7129
Bealby <mgbealby@gmail.com>
2022-10-22 07:41:50 +0200

    Setup views, urls for gallery

--- ignite-theatre-v2/about/views.py
+++ ignite-theatre-v2/about/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def about(request):
     # A view to return the about page
-    return render(request, "about.html")
+    return render(request, "about/about.html")
--- ignite-theatre-v2/gallery/views.py
+++ ignite-theatre-v2/gallery/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Image
 
-# Create your views here.
+
+def all_gallery(request):
+    '''A view to show all images'''
+    '''context = to allow things to be sent to template'''
+    
+    gallery = Image.object.all()
+
+    context = {
+        'gallery': gallery,
+    }
+
+    return render(request, 'gallery/gallery.html', context)
