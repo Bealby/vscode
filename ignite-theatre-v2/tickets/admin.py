c664a7b3be16fd95c931bdcc89c2fdffb1439c0c
Bealby <mgbealby@gmail.com>
2022-10-26 20:41:29 +0200

    Removed fields from ticket model. Set-up Admin

--- ignite-theatre-v2/tickets/admin.py
+++ ignite-theatre-v2/tickets/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Show
+from .models import Ticket
 
-# Register your models here.
+
+class ShowAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'poster',
+    )
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        'show',
+        'date',
+    )
+
+
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Ticket), TicketAdmin)
--- ignite-theatre-v2/tickets/models.py
+++ ignite-theatre-v2/tickets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+'''Pillow must be installed'''
 
-# Create your models here.
+
+class Show(models.Model):
+    
+    '''Programmatic Name'''
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    poster = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Ticket(models.Model):
+
+    show = models.ForeignKey('show', null=True, blank=True, on_delete=models.SET_NULL)
+    date = models.DateField()
+    place = models.CharField(max_length=254)
+    Location = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
